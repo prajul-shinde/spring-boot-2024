f84ceba542aec1e089d470bf3607575cd805d0d4
prajul-shinde <prajulshinde@gmail.com>
2024-03-14 13:28:46 +0530

    fixed issue: controller test were passing without authentication

--- O8-project-1/restservice/src/main/java/com/omsai/restservice/config/SpringSecConfig.java
+++ O8-project-1/restservice/src/main/java/com/omsai/restservice/config/SpringSecConfig.java
@@ -2,6 +2,7 @@ package com.omsai.restservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -11,9 +12,15 @@ public class SpringSecConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.csrf(csrf -> {
-            csrf.ignoringRequestMatchers("/api/**");
-        });
+        http.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> {
+                    authorizationManagerRequestMatcherRegistry
+                            .anyRequest()
+                            .authenticated();
+                })
+                .httpBasic(Customizer.withDefaults())
+                .csrf(csrf -> {
+                    csrf.ignoringRequestMatchers("/api/**");
+                });
 
         return http.build();
     }
